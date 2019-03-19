8befa6dbc3f7bf158750a07766c7790dda3ee1e4
Cynthia Thompson <30018117@student.toiohomai.ac.nz>
2019-03-20 09:13:19 +1300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to week2");
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}", name);
+
+            Console.Write("Could you please enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write($"Ok {name}, could you please enter your gender: ");
+            string gender = Console.ReadLine();
+
+            Console.Write("Please enter your address: ");
+            string address = Console.ReadLine();
+
+            Console.Clear();
+        }
+    }
+}
